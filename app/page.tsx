1d1e63cd4a61b995f81dabf3f40a6b945b40fb52
duncan91-ops <duncankithinji91@gmail.com>
2023-10-19 16:37:55 +0300

    create showcase section of the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -22,6 +22,47 @@ export default function Home() {
       </section>
       <section className={`${styles.content} container`}>
         <Menu />
+        <article className={styles.showcase}>
+          <div className={styles.one}>
+            <div className={styles.one__content}>
+              <h3 className={styles.one__name}>
+                <span>ZX9</span> <span>SPEAKER</span>
+              </h3>
+              <p className={styles.one__msg}>
+                Upgrade to premium speakers that are phenomenally built to
+                deliver truly remarkable sound.
+              </p>
+              <Link href="#" className={`${styles.one__btn} btn`}>
+                see product
+              </Link>
+            </div>
+          </div>
+          <div className={styles.two}>
+            <div className={styles.two__content}>
+              <h3 className={styles.two__name}>ZX7 SPEAKER</h3>
+              <Link
+                href="#"
+                className={`${styles.two__btn} btn btn__secondary`}
+              >
+                see product
+              </Link>
+            </div>
+          </div>
+          <div className={styles.three}>
+            <div className={styles.three__hero}></div>
+            <div className={styles.three__content}>
+              <div className={styles.three__details}>
+                <h3 className={styles.three__name}>YX1 EARPHONES</h3>
+                <Link
+                  href="#"
+                  className={`${styles.three__btn} btn btn__secondary`}
+                >
+                  see product
+                </Link>
+              </div>
+            </div>
+          </div>
+        </article>
         <Caption />
       </section>
     </main>
