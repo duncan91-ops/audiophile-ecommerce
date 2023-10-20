8130e974e85d5225e75a56aae383f11c243abbe1
duncan91-ops <duncankithinji91@gmail.com>
2023-10-20 21:20:32 +0300

    configure the category page component

--- app/categories/[slug]/page.tsx
+++ app/categories/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { getCategories } from "@/utils/getCategories";
 import styles from "./page.module.scss";
 import { getProducts } from "@/utils/getProducts";
@@ -13,7 +14,16 @@ export async function generateStaticParams() {
 
 export default function Category({ params }: { params: { slug: string } }) {
   let products = getProducts();
-  products = products.filter((product) => product.category === params.slug);
+  products = products
+    .filter((product) => product.category === params.slug)
+    .sort((a, b) => {
+      if (b.new && !a.new) {
+        return 1;
+      } else if (a.new && !b.new) {
+        return -1;
+      }
+      return 0;
+    });
 
   return (
     <main className={styles.main}>
@@ -21,7 +31,42 @@ export default function Category({ params }: { params: { slug: string } }) {
         <h1 className={styles.title}>{params.slug}</h1>
       </section>
       <div className={`${styles.content} container`}>
-        <section className={styles.products}></section>
+        <section className={styles.products}>
+          {products.map((product) => {
+            return (
+              <article className={styles.product} key={product.id}>
+                <div className={styles.product__img}>
+                  <picture>
+                    <source
+                      media="(min-width: 1280px)"
+                      srcSet={product.image.desktop}
+                    />
+                    <source
+                      media="(min-width: 768px)"
+                      srcSet={product.image.tablet}
+                    />
+                    <img
+                      src={product.image.mobile}
+                      alt="product image"
+                      className={styles.img}
+                    />
+                  </picture>
+                </div>
+                <div className={styles.product__details}>
+                  {product.new && <p className={styles.new}>new product</p>}
+                  <h2 className={styles.name}>{product.name}</h2>
+                  <p className={styles.description}>{product.description}</p>
+                  <Link
+                    href={`/products/${product.slug}`}
+                    className="btn btn__primary"
+                  >
+                    see product
+                  </Link>
+                </div>
+              </article>
+            );
+          })}
+        </section>
         <section className={styles.common}>
           <Menu />
           <Caption />
